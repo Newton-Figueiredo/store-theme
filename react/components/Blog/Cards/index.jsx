883eb57b3e8c4a3c64388784f78bbf42c6ef9452
Newton Figueiredo <new.figueiredo@gmail.com>
2022-04-14 12:33:08 -0300

    Update index.jsx

--- react/components/Blog/Cards/index.jsx
+++ react/components/Blog/Cards/index.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import RecipeCard from "./RecipeCard";
 const accessToken =
   "JMxsFS4i4PNnVPkGxNlwcf4m_vlsd87vvTSOzwlqRRc";
 const spaceId = "g6t519g8npzy";
@@ -40,7 +39,6 @@ export default function Cards() {
     )
       .then(res => res.json())
       .then(response => {
-        console.log("oieeee", response.data.receitasCollection.items);
         setData(response.data.receitasCollection.items);
       })
       .catch(error => {
@@ -55,16 +53,13 @@ export default function Cards() {
       <div className="recipe-list">
         {data == null ? (
           <>
-            <span>esta carregando</span>
+            <span>Loading...</span>
           </>
         ) : (
           <>
             <span>{data[0].titulo}</span>
           </>
         )}
-        {/* {recipes.map((response) => (
-        <RecipeCard key={response.sys.id} recipe={response} />
-      ))} */}
       </div>
     </>
   )
